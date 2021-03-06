8076771014ae4771559bba1899fa7980d8670631
Savannah Fortson <savannah.fortson@gmail.com>
2021-03-06 17:49:00 -0500

    first updates to js - input variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,26 @@
+// Password-Generator JS sheet
+
+// Variables for character types
+// Special characters
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+// Numbers
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Uppercase letters
+alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// Lowercase letter function (from uppercase values)
+var toLower = function (x) {
+  return x.toLowerCase();
+};
+// Lowercase letter storage as variable
+alpha2 = alpha.map(toLower);
+
+// Variables for user input types
+var enter;
+var confirmCharacter;
+var confirmNumber;
+var confirmUppercase;
+var confirmLowercase;
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
