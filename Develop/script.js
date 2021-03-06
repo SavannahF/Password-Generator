37714437e672bf311e3cc91e0753a97bf9a9b476
Savannah Fortson <savannah.fortson@gmail.com>
2021-03-06 17:51:07 -0500

    fix lower and uppercase

--- Develop/script.js
+++ Develop/script.js
@@ -6,13 +6,13 @@ character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/
 // Numbers
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Uppercase letters
-alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 // Lowercase letter function (from uppercase values)
 var toLower = function (x) {
   return x.toLowerCase();
 };
 // Lowercase letter storage as variable
-alpha2 = alpha.map(toLower);
+lower = upper.map(toLower);
 
 // Variables for user input types
 var enter;
