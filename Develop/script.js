5c2888cf3548370babd1fe6ef30c9f6b4c74f601
Savannah Fortson <savannah.fortson@gmail.com>
2021-03-06 20:26:57 -0500

    making sure I committed the variable arrays

--- Develop/script.js
+++ Develop/script.js
@@ -26,26 +26,6 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// After clicking the button...
-function generatePassword() {
-  // Asks for user input
-  enter = parseInt(prompt("Please choose the number of characters for your password (must be between 8 - 128)."));
-  // User doesn't enter a value?
-  if (!enter) {
-      alert("Enter a number between 8 and 128.");
-  } 
-  else if (enter < 8 || enter > 128) {
-      // Must enter the correct number or else
-      enter = parseInt(prompt("Please choose a number between 8 and 128."));
-  } 
-  else {
-      // Pops up after valid number entered; must click 
-      confirmNumber = confirm("Would you like numbers?");
-      confirmCharacter = confirm("What about special characters?");
-      confirmUppercase = confirm("Would you like Uppercase letters?");
-      confirmLowercase = confirm("And finally, what about lowercase letters?");
-  };
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
